06858867ffa1e966a357b98c2877138b7fefefca
liri <liri_1024@163.com>
2018-12-14 00:20:53 +0800

    重构

--- src/main/java/spider/Test.java
+++ src/main/java/spider/Test.java
@@ -15,53 +15,34 @@ public class Test implements PageProcessor {
     // process是定制爬虫逻辑的核心接口，在这里编写抽取逻辑
     public void process(Page page) {
         // 部分二：定义如何抽取页面信息，并保存下来
-       /* List<String> list =   page.getHtml().xpath("/head/html()").all();
+       List<String> list =   page.getHtml().xpath("*/head/html()").all();
         page.putField("head",list);
 
-        List<String> bodyList =   page.getHtml().xpath("/body/html()").all();
-        page.putField("body",bodyList);*/
+        List<String> bodyList =   page.getHtml().xpath("*/body/html()").all();
+        page.putField("body",bodyList);
 
-        List<String> scriptList =    page.getHtml().xpath("*/script").all();
-        /*page.putField("scriptList",scriptList);*/
+        String columnList =   page.getHtml().xpath("*/li[@class='active']/a/text()").toString();
+        page.putField("columnList",columnList);
 
-        if (scriptList.isEmpty()) {
+        page.putField("columnList2",page.getHtml().xpath("*/div[@class='play_nav hidden-xs']/a/text()").all());
+
+       String titleList =   page.getHtml().xpath("*/div[@class='player_title']/h1/html()").toString();
+        page.putField("titleList",titleList);
+
+
+        if (titleList.isEmpty()) {
+            System.out.println("s==============跳过");
             page.setSkip(true);
         }else {
-            for (String s:scriptList){
-                String downurls = subString(s,"var downurls = \"","#");
-                if (!downurls.equals("not")){
-                    System.out.println("====================================0"+downurls);
-                    System.out.println("====================================1"+downurls.substring(0,downurls.indexOf("https")));
-                    System.out.println("====================================2"+downurls.substring(downurls.indexOf("https"),downurls.length()));
-                }
-            }
+                System.out.println("s=============="+titleList);
         }
 
 
         // 部分三：从页面发现后续的url地址来抓取
-        List<String> list = page.getHtml().xpath("*/a/@href").all();
+       /* List<String> list = page.getHtml().xpath("/a/@href").all();
         System.out.println("url-------------->"+list.size());
-        page.addTargetRequests(page.getHtml().xpath("*/a/@href").all());
+        page.addTargetRequests(page.getHtml().xpath("/a/@href").all());*/
     }
-
-    public static String subString(String str, String strStart, String strEnd) {
-
-        /* 找出指定的2个字符在 该字符串里面的 位置 */
-        int strStartIndex = str.indexOf(strStart);
-        int strEndIndex = str.indexOf(strEnd);
-
-        /* index 为负数 即表示该字符串中 没有该字符 */
-        if (strStartIndex < 0) {
-            return "not";
-        }
-        if (strEndIndex < 0) {
-            return "not";
-        }
-        /* 开始截取 */
-        String result = str.substring(strStartIndex, strEndIndex).substring(strStart.length());
-        return result;
-    }
-
 
 
     @Override
@@ -73,7 +54,7 @@ public class Test implements PageProcessor {
 
         Spider.create(new Test())
                 //从"https://github.com/code4craft"开始抓
-                .addUrl("https://www.885nu.com/vod/29/11689-1.html")
+                .addUrl("https://www.886er.com/vod/17/11675-1.html")
                 //开启5个线程抓取
                 .thread(5)
                 //启动爬虫
