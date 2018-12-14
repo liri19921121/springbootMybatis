67a6cd8ddd630aae2a39490e71b82c85467c29d9
sun <riLI498717728>
2018-12-14 10:57:16 +0800

    重构

--- src/main/java/com/common/utils/ImgUtil.java
+++ src/main/java/com/common/utils/ImgUtil.java
@@ -1,16 +1,19 @@
 package com.common.utils;
 
 import org.apache.commons.io.FileUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 @Component
 public class ImgUtil {
-	
-	
+
+	@Value("${upload.root.path}")
+	public static String root_fold;
+
 	 /**
 	 * 上传文件
 	 * @param filePath 文件名 
@@ -88,5 +91,82 @@ public class ImgUtil {
 		pics = pics.replace("data:image/x-icon;base64,", "");
 		pics = pics.replace("data:image/gif;base64,", "");
 		return pics;
+	}
+
+	public static void main(String[] args) throws Exception {
+		String imgUrl = "https://gss0.bdstatic.com/5bVWsj_p_tVS5dKfpU_Y_D3/res/r/image/2018-12-13/53b9dada603935ee60d089c6ca53d66a.png";
+		//获得图片名字
+		String imgName = imgUrl.substring(imgUrl.lastIndexOf("/"),imgUrl.length());
+		//new一个URL对象
+		URL url = new URL(imgUrl);
+		//打开链接
+		HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+		//设置请求方式为"GET"
+		conn.setRequestMethod("GET");
+		//超时响应时间为5秒
+		conn.setConnectTimeout(5 * 1000);
+		//通过输入流获取图片数据
+		InputStream inStream = conn.getInputStream();
+		//得到图片的二进制数据，以二进制封装得到数据，具有通用性
+		byte[] data = readInputStream(inStream);
+		//new一个文件对象用来保存图片，默认保存当前工程根目录
+		createFile(root_fold);
+		File imageFile = new File(root_fold+imgName);
+		//创建输出流
+		FileOutputStream outStream = new FileOutputStream(imageFile);
+		//写入数据
+		outStream.write(data);
+		//关闭输出流
+		outStream.close();
+	}
+
+	//把文件保存在本地
+	public static String saveLocal(String imgUrl){
+		try {
+			//获得图片名字
+			String imgName = imgUrl.substring(imgUrl.lastIndexOf("/")+1,imgUrl.length());
+			//new一个URL对象
+			URL url = new URL(imgUrl);
+			//打开链接
+			HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+			//设置请求方式为"GET"
+			conn.setRequestMethod("GET");
+			//超时响应时间为5秒
+			conn.setConnectTimeout(5 * 1000);
+			//通过输入流获取图片数据
+			InputStream inStream = conn.getInputStream();
+			//得到图片的二进制数据，以二进制封装得到数据，具有通用性
+			byte[] data = readInputStream(inStream);
+			//new一个文件对象用来保存图片，默认保存当前工程根目录
+			createFile(root_fold);
+			File imageFile = new File(root_fold+imgName);
+			//创建输出流
+			FileOutputStream outStream = new FileOutputStream(imageFile);
+			//写入数据
+			outStream.write(data);
+			//关闭输出流
+			outStream.close();
+			return imgName;
+		}catch (Exception e){
+			return null;
+		}
+	}
+
+
+	public static byte[] readInputStream(InputStream inStream) throws Exception{
+		ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+		//创建一个Buffer字符串
+		byte[] buffer = new byte[1024];
+		//每次读取的字符串长度，如果为-1，代表全部读取完毕
+		int len = 0;
+		//使用一个输入流从buffer里把数据读取出来
+		while( (len=inStream.read(buffer)) != -1 ){
+			//用输出流往buffer里写入数据，中间参数代表从哪个位置开始读，len代表读取的长度
+			outStream.write(buffer, 0, len);
+		}
+		//关闭输入流
+		inStream.close();
+		//把outStream里的数据写入内存
+		return outStream.toByteArray();
 	}
 }
--- src/main/java/spider/Test.java
+++ src/main/java/spider/Test.java
@@ -8,34 +8,40 @@ import us.codecraft.webmagic.processor.PageProcessor;
 import java.util.List;
 
 public class Test implements PageProcessor {
-    // 部分一：抓取网站的相关配置，包括编码、抓取间隔、重试次数等
     private Site site = Site.me().setRetryTimes(3).setSleepTime(1000);
 
     @Override
-    // process是定制爬虫逻辑的核心接口，在这里编写抽取逻辑
     public void process(Page page) {
-        // 部分二：定义如何抽取页面信息，并保存下来
-       List<String> list =   page.getHtml().xpath("*/head/html()").all();
+        List<String> list =   page.getHtml().xpath("*/html/html()").all();
+        page.putField("html",list);
+
+        List<String> imgList =   page.getHtml().xpath("*/body/li[@class='swiper-slide']/text()").all();
+        page.putField("imgList",imgList);
+
+        List<String> gifList =   page.getHtml().xpath("*/div[@class='box-video-text-list']/text()").all();
+        page.putField("gifList",gifList);
+
+/*       List<String> list =   page.getHtml().xpath("/head/html()").all();
         page.putField("head",list);
 
-        List<String> bodyList =   page.getHtml().xpath("*/body/html()").all();
-        page.putField("body",bodyList);
+        List<String> bodyList =   page.getHtml().xpath("/body/html()").all();
+        page.putField("body",bodyList);*/
 
-        String columnList =   page.getHtml().xpath("*/li[@class='active']/a/text()").toString();
-        page.putField("columnList",columnList);
+        /*String columnList =   page.getHtml().xpath("/li[@class='active']/a/text()").toString();
+        page.putField("column",columnList);
 
-        page.putField("columnList2",page.getHtml().xpath("*/div[@class='play_nav hidden-xs']/a/text()").all());
+        page.putField("columnFull",page.getHtml().xpath("/div[@class='play_nav hidden-xs']/a/text()").all());
 
-       String titleList =   page.getHtml().xpath("*/div[@class='player_title']/h1/html()").toString();
-        page.putField("titleList",titleList);
+       String titleList =   page.getHtml().xpath("/div[@class='player_title']/h1/html()").toString();
+        page.putField("titleList",titleList);*/
 
 
-        if (titleList.isEmpty()) {
+        /*if (imgList.isEmpty()) {
             System.out.println("s==============跳过");
             page.setSkip(true);
         }else {
-                System.out.println("s=============="+titleList);
-        }
+                System.out.println("s=============="+imgList);
+        }*/
 
 
         // 部分三：从页面发现后续的url地址来抓取
