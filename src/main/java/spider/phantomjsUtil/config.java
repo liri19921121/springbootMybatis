ba2402b8bbe54904e6e3109144d7ad889c0b818b
sun <riLI498717728>
2018-12-21 18:28:00 +0800

    重构

--- src/main/java/spider/phantomjsUtil/config.java
+++ src/main/java/spider/phantomjsUtil/config.java
@@ -1,32 +1,18 @@
 package spider.phantomjsUtil;
 
 
-import java.util.Properties;
+import com.common.utils.SpiderUtils;
 
 public class config {
 
 
     public static String getConfigFile(){
-        if (isOSLinux()){
+        if (SpiderUtils.isOSLinux()){
             System.out.println("jar包===："+"/home/phantomjs/config.ini");
             return "/home/phantomjs/config.ini";
         }else{
             System.out.println("jar包====："+"F:/phantomjs/phantomjs-2.1.1-windows/bin/config.ini");
             return "F:/phantomjs/phantomjs-2.1.1-windows/bin/config.ini";
-        }
-
-    }
-    /**
-     * linux返回true
-     * @return
-     */
-    public static boolean isOSLinux() {
-        Properties prop = System.getProperties();
-        String os = prop.getProperty("os.name");
-        if (os != null && os.toLowerCase().indexOf("linux") > -1) {
-            return true;
-        } else {
-            return false;
         }
     }
 
